8a6a0192c089d3aca7a7b067bea4f9abde1a8c01
Roy Vanegas <78804445+Derekdel989@users.noreply.github.com>
2023-03-30 21:29:24 -0400

    Added to Gulpfile and CSS
    Added to gulpfile and CSS

---
+++ gulpfile.js
@@ -0,0 +1,180 @@
+const { src, dest, series, watch } = require(`gulp`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload,
+    CSSLinter = require(`gulp-stylelint`),
+    jsLinter = require(`gulp-eslint`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    jsCompressor = require(`gulp-uglify`),
+    sass = require(`gulp-sass`)(require(`sass`)),
+    babel = require(`gulp-babel`);
+
+let browserChoice = `default`;
+
+async function brave () {
+    browserChoice = `brave browser`;
+}
+
+async function chrome () {
+    browserChoice = `google chrome`;
+}
+
+async function edge () {
+    // In Windows, the value might need to be “microsoft-edge”. Note the dash.
+    browserChoice = `microsoft edge`;
+}
+
+async function firefox () {
+    browserChoice = `firefox`;
+}
+
+async function opera () {
+    browserChoice = `opera`;
+}
+
+async function safari () {
+    browserChoice = `safari`;
+}
+
+async function vivaldi () {
+    browserChoice = `vivaldi`;
+}
+
+async function allBrowsers () {
+    browserChoice = [
+        `brave browser`,
+        `google chrome`,
+        `microsoft edge`, // Note: In Windows, this might need to be microsoft-edge
+        `firefox`,
+        `opera`,
+        `safari`,
+        `vivaldi`
+    ];
+}
+
+let lintCSS = () => {
+    return src(`styles/css/**/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
+let lintJS = () => {
+    return src(`scripts/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let compressHTML = () => {
+    return src([`*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compileCSSForProd = () => {
+    return src(`styles/main.css`)
+        .pipe(sass.sync({
+            outputStyle: `compressed`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`prod/styles`));
+};
+
+let transpileJSForProd = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let copyUnprocessedAssetsForProd = () => {
+    return src([
+        `img*/*.jpg`,       // Source all jpg images,
+        `img*/*.svg`,       // and all svg images,
+        `json*/*.json`,     // and all .json,
+        `styles*/reset.css`,    // and the one .css,
+    ], {dot: true})
+        .pipe(dest(`prod`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+            ]
+        }
+    });
+
+    watch(`js/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`styles/*.css`, series(lintCSS, copyUnprocessedAssetsForDev))
+        .on(`change`, reload);
+
+};
+
+let compileCSSForDev = () => {
+    return src(`dev/styles/scss/main.scss`)
+        .pipe(sass.sync({
+            outputStyle: `expanded`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`temp/styles`));
+};
+let copyUnprocessedAssetsForDev = () => {
+    return src([
+        `img*/*.jpg`,       // Source all jpg images,
+        `img*/*.svg`,       // and all svg images,
+        `json*/*.json`,     // and all .json,
+        `styles*/*.css`,    // and all .css,
+        `index.html`        // and index.html
+    ], {dot: true})
+        .pipe(dest(`temp`));
+};
+
+exports.brave = series(brave, serve);
+exports.chrome = series(chrome, serve);
+exports.edge = series(edge, serve);
+exports.firefox = series(firefox, serve);
+exports.opera = series(opera, serve);
+exports.safari = series(safari, serve);
+exports.vivaldi = series(vivaldi, serve);
+exports.allBrowsers = series(allBrowsers, serve);
+//dev
+exports.lintJS = lintJS;
+exports.lintCSS = lintCSS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compileCSSForDev = compileCSSForDev;
+//prod
+exports.compressHTML = compressHTML;
+exports.compileCSSForProd = compileCSSForProd;
+exports.transpileJSForProd = transpileJSForProd;
+exports.copyUnprocessedAssetsForProd = copyUnprocessedAssetsForProd;
+//dev
+exports.default = series(
+    lintCSS,
+    lintJS,
+    transpileJSForDev,
+    copyUnprocessedAssetsForDev,
+    serve
+
+);
+//prod
+exports.build = series(
+    compressHTML,
+    compileCSSForProd,
+    transpileJSForProd,
+    copyUnprocessedAssetsForProd
+);
